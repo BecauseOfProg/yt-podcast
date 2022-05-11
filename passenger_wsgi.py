f606b0b5c632bd57bcd7cf99156df9af05dd0fb0
Avi Alkalay <avibrazil@gmail.com>
2022-05-11 14:49:47 -0700

    support for DreamHost; generic domain on feed.xml

---
+++ passenger_wsgi.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+# Required to make it run on DreamHost
+
+import os
+import sys
+
+INTERP = "/usr/bin/python3"
+if sys.executable != INTERP: os.execl(INTERP, INTERP, *sys.argv)
+
+sys.path.append(os.getcwd())
+
+import views.channel
+import views.download
+import views.index
+
+from app import app as application
