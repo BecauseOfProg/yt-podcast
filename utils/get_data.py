5208a4172b0d78025c0ce337ef7c981faae3428a
Avi Alkalay <avibrazil@gmail.com>
2022-08-06 02:57:37 -0700

    attempts to work YouTube customUrl

--- utils/get_data.py
+++ utils/get_data.py
@@ -2,10 +2,25 @@ from utils.youtube_api_call import yt_api_call
 
 
 def get_channel_data(id_type, user):
-    '''Return a dictionary of channel data from a channel id or username.'''
+    '''Return a dictionary of channel data from a channel id, username or custom URL.'''
+    if id_type == 'custom':
+        guess_channel = yt_api_call(
+            path      = 'search',
+            part      = 'snippet',
+            id_type   = 'q',
+            id_value  = user
+        )
+
+        # Get first response and prepare classical route
+        user = guess_channel.items[0].id.channelId
+        id_type = 'channel'
+
     id_type = 'id' if id_type == 'channel' else 'forUsername'
+
     channel_data = yt_api_call(
-        'channels', 'contentDetails,snippet', id_type, user)
+        'channels', 'contentDetails,snippet', id_type, user
+    )
+
     return channel_data
 
 
