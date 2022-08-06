ac5a27196708d10edb5039e370ded45fddb16595
Avi Alkalay <avibrazil@gmail.com>
2022-08-06 07:31:30 -0300

    support for YouTube customUrl as «youtube.com/c/{some_custom_url}»

--- utils/get_data.py
+++ utils/get_data.py
@@ -4,6 +4,7 @@ from utils.youtube_api_call import yt_api_call
 def get_channel_data(id_type, user):
     '''Return a dictionary of channel data from a channel id, username or custom URL.'''
     if id_type == 'custom':
+        # Search channels that might have custom part of URL in the snippet
         guess_channel = yt_api_call(
             path      = 'search',
             part      = 'snippet',
@@ -11,8 +12,25 @@ def get_channel_data(id_type, user):
             id_value  = user
         )
 
-        # Get first response and prepare classical route
-        user = guess_channel.items[0].id.channelId
+        # Get only returned IDs
+        cIDs = [item['id']['channelId'] for item in guess_channel['items']]
+
+        # Get more details of all returned channels
+        guess_channel = yt_api_call(
+            path      = 'channels',
+            part      = 'snippet',
+            id_type   = 'id',
+            id_value  = ','.join(cIDs)
+        )
+
+        # Search for exact customUrl match in all channel details and set user variable
+        for item in guess_channel['items']:
+            if "customUrl" in item['snippet']:
+                if user.casefold()==item['snippet']["customUrl"].casefold():
+                    user = item['id']
+                    break
+
+        # Variable user now has a channel ID; prepare for classical route
         id_type = 'channel'
 
     id_type = 'id' if id_type == 'channel' else 'forUsername'
